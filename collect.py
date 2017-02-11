40468e1e26d394ae524f0d1e6ce64030818faff2
Jun Ki Lee <joongee@gmail.com>
2017-02-11 15:22:01 -0500

    first commit

---
+++ collect.py
@@ -0,0 +1,34 @@
+# vim: tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+import sys
+import gym
+import time
+import os
+
+def collect(env, steps, f, is_render=True):
+    for _ in range(steps):
+      if is_render:
+        env.render()
+      new_observation, reward, done, _ = env.step(env.action_space.sample())
+      f.write("{},{},{}\n".format(','.join(map(str,new_observation)), reward, done))
+      f.flush()
+      if done:
+        env.reset()
+
+if __name__ == "__main__":
+  data_dir = "data"
+  is_display = False
+  post_fix = ""
+  if len(sys.argv) > 1:
+    post_fix = sys.argv[1]
+  train_filename = os.path.join(data_dir, 'train_' + post_fix +'.csv')
+  test_filename = os.path.join(data_dir, 'test_' + post_fix +'.csv')
+
+  env = gym.make('CartPole-v0')
+  env.seed(int(time.time()))
+  env.reset()
+  with open(train_filename, 'w') as f:
+    collect(env, 5000, f, is_display)
+  with open(test_filename, 'w') as f:
+    collect(env, 1000, f, is_display)
+
+
